40538b81b4b1418d0bf025c3ff344dc4e8ab6636
Fırat Temel <firattml@gmail.com>
2021-05-14 13:22:01 +0300

    added initCookieConsent

--- src/cookieHelper.js
+++ src/cookieHelper.js
@@ -1,5 +1,14 @@
 import Cookies from 'js-cookie'
 
+
+export function refresh(key){
+    let cookie = getCookie(key);
+    console.log(cookie);
+    if(cookie){
+        setCookie(key,cookie);
+    }
+}
+
 export function setCookie(key,value){
     Cookies.set(key, value , { expires: 36500, secure: true });
 }
